21dde406e0ce7be490e8665410004df8518217d4
Konika Nahar <konika.nahar@gmail.com>
2025-03-06 09:24:38 -0600

    start of react on the front end

--- src/blocks/staff-list/BlockSettings.js
+++ src/blocks/staff-list/BlockSettings.js
@@ -8,27 +8,27 @@ export default function BlockSettings({attributes, setAttributes}) {
 		const [ hasFixedBackground, setHasFixedBackground ] = useState( false );
 
 		return (
-			<InspectorControls>
-				<PanelColorSettings
-					title="Colors"
-					colorSettings={[
-						{
-							value: attributes.cardColor,
-							onChange: cardColor => setAttributes({cardColor}),
-							label: 'Card Color',
-						},
-						{
-							value: attributes.headingColor,
-							onChange: headingColor => setAttributes({headingColor}),
-							label: 'Heading Color',
-						},
-						{
-							value: attributes.textColor,
-							onChange: textColor => setAttributes({textColor}),
-							label: 'Text Color',
-						},
-					]}
-				/>
-			</InspectorControls>
+		<InspectorControls>
+			<PanelColorSettings
+				title="Colors"
+				colorSettings={[
+					{
+						value: attributes.cardColor,
+						onChange: cardColor => setAttributes({cardColor}),
+						label: 'Card Color',
+					},
+					{
+						value: attributes.headingColor,
+						onChange: headingColor => setAttributes({headingColor}),
+						label: 'Heading Color',
+					},
+					{
+						value: attributes.textColor,
+						onChange: textColor => setAttributes({textColor}),
+						label: 'Text Color',
+					},
+				]}
+			/>
+		</InspectorControls>
 	)
 }
